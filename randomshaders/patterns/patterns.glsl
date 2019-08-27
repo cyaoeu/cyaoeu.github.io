b315131e18aff6a3d24fb3c8581aa6987e68147b
Henrik Berglund <cyaoeu3d@gmail.com>
2019-08-27 16:32:44 +0200

    Add new shader

---
+++ randomshaders/patterns/patterns.glsl
@@ -0,0 +1,104 @@
+/*
+// Author @patriciogv ( patriciogonzalezvivo.com ) - 2015
+
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform vec2 u_resolution;
+uniform float u_time;
+
+#define PI 3.14159265358979323846
+
+vec2 rotate2D(vec2 _st, float _angle){
+    _st -= 0.5;
+    _st =  mat2(cos(_angle),-sin(_angle),
+                sin(_angle),cos(_angle)) * _st;
+    _st += 0.5;
+    return _st;
+}
+
+vec2 tile(vec2 _st, float _zoom){
+    _st *= _zoom;
+    return fract(_st);
+}
+
+float box(vec2 _st, vec2 _size, float _smoothEdges){
+    _size = vec2(0.5)-_size*0.5;
+    vec2 aa = vec2(_smoothEdges*0.5);
+    vec2 uv = smoothstep(_size,_size+aa,_st);
+    uv *= smoothstep(_size,_size+aa,vec2(1.0)-_st);
+    return uv.x*uv.y;
+}
+
+void main(void){
+    vec2 st = gl_FragCoord.xy/u_resolution.xy;
+    vec3 color = vec3(0.0);
+    vec3 color2 = vec3(0.0);
+
+    // Divide the space in 4
+    st = tile(st,4.);
+
+    // Use a matrix to rotate the space 45 degrees
+    st = rotate2D(st,PI*0.25);
+    //st = rotate2D(st,u_time*PI*0.25);
+
+    // Draw a square
+    color = vec3(box(st,vec2(0.7),0.01));
+    color2 = vec3(box(st,vec2(0.67),0.2));
+    // color = vec3(st,0.0);
+
+    gl_FragColor = vec4(color,1.0);
+}
+
+*/
+
+// Author @patriciogv ( patriciogonzalezvivo.com ) - 2015
+
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform vec2 u_resolution;
+uniform float u_time;
+
+vec2 brickTile(vec2 _st, float _zoom){
+    _st *= _zoom;
+
+    // Here is where the offset is happening
+    _st.x += step(1., mod(-_st.y,2.))* -1.*u_time*step(1., mod(u_time,2.));
+    _st.x += step(1., mod(_st.y,2.)) * 1.*u_time*step(1., mod(u_time,2.));
+
+    _st.y += step(1., mod(-_st.x,2.))*  -1.*u_time*step(1., mod(-u_time,2.));
+    _st.y += step(1., mod(_st.x,2.)) *1.*u_time*step(1., mod(-u_time,2.));
+    
+
+    return fract(_st);
+}
+
+float box(vec2 _st, vec2 _size){
+    _size = vec2(0.5)-_size*0.5;
+    vec2 uv = smoothstep(_size,_size+vec2(1e-4),_st);
+    uv *= smoothstep(_size,_size+vec2(1e-4),vec2(1.0)-_st);
+    return uv.x*uv.y;
+}
+
+void main(void){
+    vec2 st = gl_FragCoord.xy/u_resolution.xy;
+    st = st *2.-1.;
+    vec3 color = vec3(0.0);
+
+    // Modern metric brick of 215mm x 102.5mm x 65mm
+    // http://www.jaharrison.me.uk/Brickwork/Sizes.html
+     //st /= vec2(2.15,0.65)/1.5;
+
+    // Apply the brick tiling
+    st = brickTile(st,10.0*(1.*sin(u_time)));
+
+    color = vec3(box(st,vec2(0.9)));
+
+    // Uncomment to see the space coordinates
+    // color = vec3(st,0.0);
+
+    gl_FragColor = vec4(color,1.0);
+}
